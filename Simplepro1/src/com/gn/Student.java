7d4bb390daed5a89560fc814c51dc7ba46146789
gopal <gopal@DESKTOP-UM3LNP0>
2018-06-15 00:23:43 -0700

    change1

---
+++ Simplepro1/src/com/gn/Student.java
@@ -0,0 +1,28 @@
+package com.gn;
+
+class Student {
+	String name;
+	int rollno;
+	
+
+	public Student(String name, int rollno) {
+		super();
+		this.name = name;
+		this.rollno = rollno;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", rollno=" + rollno + "]";
+	}
+
+
+	public static void main(String[] args) {
+		Student s1=new Student("gopal", 101);
+		
+		Student s2=new Student("preety", 102);
+		System.out.println(s1);
+	}
+
+}
